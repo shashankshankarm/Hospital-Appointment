36388d4836fb2adc4d53f779403d5b3f5f1e93ac
Shashank Shankar M <151534459+shashankshankarm@users.noreply.github.com>
2026-04-19 18:23:53 +0530

    Add files via upload

---
+++ HospitalAppointment/HospitalAppointment/src/test/java/com/hospitalappointment/HospitalAppointmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hospitalappointment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalAppointmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
